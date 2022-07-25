471255ca17de42f3609be84931b7cd9da2f3a65b
DenisseCardonaR <karedenisse1@gmail.com>
2022-07-25 15:45:01 -0500

    version1

--- admin/venta/venta.js
+++ admin/venta/venta.js
@@ -46,3 +46,9 @@ function mandarConfirmacionEliminar()
 {
     alert("Se elimino la venta");
 }
+
+function generarClaveUnicaVenta() {
+    let clave_unica_venta = "OQV-"+Math.floor(Math.random()*999999);;
+    
+     document.getElementById("txtClaveUnicaVenta").value = clave_unica_venta;
+}
